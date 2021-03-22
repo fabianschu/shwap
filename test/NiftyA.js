bd31c50da7e02ae963bcdfea9edf425c9976e161
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2021-03-22 23:49:01 +0100

    test - ownership transfer

--- test/NiftyA.js
+++ test/NiftyA.js
@@ -12,10 +12,22 @@ describe("NiftyA contract", async () => {
   });
 
   it("mints a token to the owner", async function () {
-    const { owner, alice, bob } = await getNamedAccounts();
+    const { owner } = await getNamedAccounts();
     const niftyAInstance = await ethers.getContract("NiftyA", owner);
     const ownerBalance = await niftyAInstance.balanceOf(owner);
 
     expect(ownerBalance).to.equal(1);
+  });
+
+  it("can transfer ownership from owner to alice to bob", async function () {
+    const [owner, alice, bob] = await ethers.getSigners();
+    const niftyAInstance = await ethers.getContract("NiftyA", owner.address);
+    await niftyAInstance.transferFrom(owner.address, alice.address, 1);
+    await niftyAInstance
+      .connect(alice)
+      .transferFrom(alice.address, bob.address, 1);
+    const bobBalance = await niftyAInstance.balanceOf(bob.address);
+    expect(bobBalance).to.equal(1);
+    expect(await niftyAInstance.ownerOf(1)).to.equal(bob.address);
   });
 });
