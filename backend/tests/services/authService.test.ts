fb2dd7528a1a9c820ea06d973128348afe8fe6d9
Fabian Scherer <48454910+fabianschu@users.noreply.github.com>
2021-04-19 02:03:00 +0200

    references user for proposals if avail

--- backend/tests/services/authService.test.ts
+++ backend/tests/services/authService.test.ts
@@ -8,9 +8,10 @@ import { privateToAddress, ecsign, hashPersonalMessage } from "ethereumjs-util";
 import AuthService from "../../src/services/authService";
 import { User } from "../../src/entity/User";
 
-const hexString =
-  "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3";
-const privateKey = Buffer.from(hexString, "hex");
+const privateKey = Buffer.from(
+  "c87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
+  "hex"
+);
 const publicAddress = privateToAddress(privateKey).toString("hex");
 
 const signMsg = (msg: string, privKey: Buffer) => {
